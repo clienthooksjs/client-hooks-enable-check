2ab134889d1b73b8c14fab9383db8476cf3293b4
jieling.guan <jieling.guan@jimu.com>
2016-08-18 18:32:19 +0800

    add

--- index.js
+++ index.js
@@ -7,16 +7,29 @@ const path = require('path');
 class EnableCheckPlugin {
   constructor({ cwd = '' } = {}) {
     this[Symbol.for('initProcess')]();
-  }
-
-  [Symbol.for('initProcess')]() {
-    const modulePath = path.join(__dirname, 'enable-check.js');
-
-    this[Symbol.for('process')] = fork(modulePath);
+    this[Symbol.for('bindProcessExitCallback')]();
   }
 
   getProcess() {
     return this[Symbol.for('process')];
+  }
+
+  [Symbol.for('initProcess')]() {
+    const modulePath = path.join(__dirname, 'check.js');
+
+    this[Symbol.for('process')] = fork(modulePath, [], { silent: true });
+  }
+
+  [Symbol.for('bindProcessExitCallback')]() {
+    const modulePath = path.join(__dirname, 'enable-check.js');
+
+    const childProcess = getProcess();
+
+    childProcess.on('message', (message) => {
+      if (message.title === 'exit') {
+        fork(modulePath, [], { silent: true });
+      }
+    });
   }
 };
 
